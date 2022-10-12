554f08dd877672f662759c9183e57b162156f581
ravyaryo <ravyaaryo@gmail.com>
2022-10-12 23:08:10 +0700

    Initial Commit

---
+++ MyFirstAppWithSwift/MyFirstAppWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppWithSwiftApp.swift
+//  MyFirstAppWithSwift
+//
+//  Created by Ravy Aryo on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
